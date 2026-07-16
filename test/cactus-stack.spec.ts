46925bca42444b26c190e545248ea6e7f5b7dd91
kei-g <km.8k6ce+github@gmail.com>
2026-07-16 10:41:22 +0900

    :building_construction: Migrate testing framework from `mocha` to Node.js test runner
    Signed-off-by: kei-g <km.8k6ce+github@gmail.com>

--- test/cactus-stack.spec.ts
+++ test/cactus-stack.spec.ts
@@ -1,5 +1,5 @@
 import { CactusStack, CactusStackError } from '../src/index.ts'
-import { describe, it } from 'mocha'
+import { describe, it } from 'node:test'
 import { equal } from 'node:assert'
 
 describe(
